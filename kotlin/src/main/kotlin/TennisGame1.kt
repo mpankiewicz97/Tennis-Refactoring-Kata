c74626fc81dac5fafcd5530f328faf00b5d7b651
Miko Pankiewicz <mikolaj.pankiewicz@deliveryhero.com>
2023-04-20 17:31:00 +0200

    pairing 4/20/23 - refactoring to include specific names for various test scores

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,51 +1,65 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var player1Score: Int = 0
+    private var player2Score: Int = 0
 
     override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            m_score1 += 1
-        else
-            m_score2 += 1
+        if (playerName === "player1") {
+            player1Score += 1
+        } else {
+            player2Score += 1
+        }
     }
 
     override fun getScore(): String {
-        var score = ""
-        var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            if (minusResult == 1)
-                score = "Advantage player1"
-            else if (minusResult == -1)
-                score = "Advantage player2"
-            else if (minusResult >= 2)
-                score = "Win for player1"
-            else
-                score = "Win for player2"
+        return if (player1Score == player2Score) {
+            getScoreForEven()
         } else {
-            for (i in 1..2) {
-                if (i == 1)
-                    tempScore = m_score1
-                else {
-                    score += "-"
-                    tempScore = m_score2
-                }
-                when (tempScore) {
-                    0 -> score += "Love"
-                    1 -> score += "Fifteen"
-                    2 -> score += "Thirty"
-                    3 -> score += "Forty"
-                }
+            getScoreForUneven()
+        }
+    }
+
+    private fun getScoreForUneven(): String {
+        return if (player1Score >= 4 || player2Score >= 4) {
+            val minusResult = player1Score - player2Score
+            if (minusResult == 1) {
+                "Advantage player1"
+            } else if (minusResult == -1) {
+                "Advantage player2"
+            } else if (minusResult >= 2) {
+                "Win for player1"
+            } else {
+                "Win for player2"
+            }
+        } else {
+            getScoreForRunningGame()
+        }
+    }
+
+    private fun getScoreForRunningGame(): String {
+        var tempScore1: Int
+        var score1 = ""
+        for (i in 1..2) {
+            if (i == 1) {
+                tempScore1 = player1Score
+            } else {
+                score1 += "-"
+                tempScore1 = player2Score
+            }
+            when (tempScore1) {
+                0 -> score1 += "Love"
+                1 -> score1 += "Fifteen"
+                2 -> score1 += "Thirty"
+                3 -> score1 += "Forty"
             }
         }
-        return score
+        return score1
+    }
+
+    private fun getScoreForEven() = when (player1Score) {
+        0 -> "Love-All"
+        1 -> "Fifteen-All"
+        2 -> "Thirty-All"
+        else -> "Deuce"
     }
 }
